241c4743ca701ba67f3fcba2ca1fce11b63759c6
Ryan Sise <kerneltux0@gmail.com>
2019-03-01 18:43:43 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*data)
+    #binding.pry
+	  zoo = []
+    self.each do |key,value|
+      #binding.pry
+      if data.include?(value)
+        zoo << key
+			end
+		end
+		zoo
   end
 end
